5db7837b8ca9ff73cdab2e1403eb46ab4da2bb15
Kush Rawat <rawatkush0704@gmail.com>
2022-01-13 16:31:36 +0530

    middleware

--- index.js
+++ index.js
@@ -5,7 +5,23 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
-dotenv.config()
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
+  console.log("Connected to MongoDB");
+});
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to homepage");
+});
+app.get("/users", (req, res) => {
+  res.send("Welcome to user page");
+});
 
 app.listen(8800, () => {
   console.log("Backend Server is running!");
