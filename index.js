31b65d60b2e45adca926cbd31fe0c4852db7d923
Kush Rawat <rawatkush0704@gmail.com>
2022-01-13 19:25:58 +0530

    user router created

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
 
 dotenv.config();
 
@@ -16,12 +18,8 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.get("/", (req, res) => {
-  res.send("Welcome to homepage");
-});
-app.get("/users", (req, res) => {
-  res.send("Welcome to user page");
-});
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
 
 app.listen(8800, () => {
   console.log("Backend Server is running!");
