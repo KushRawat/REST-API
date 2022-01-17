8e32cc0a30b09a1e90fd3d9150cabb6dd28eb7c3
Kush Rawat <rawatkush0704@gmail.com>
2022-01-18 00:47:38 +0530

    crud completed

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
 const Posts = require("../models/Post");
+const User = require("../models/User");
 
 // create a post
 router.post("/", async (req, res) => {
@@ -59,12 +60,25 @@ router.put("/:id/like", async (req, res) => {
 router.get("/:id", async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
-        res.status(200).json(post)
+        res.status(200).json(post);
     } catch (err) {
         res.status(500).json(err);
     }
 });
-// get all posts(timline)
-
+// get all posts(timeline)
+router.get("/timeline/all", async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({ userId: currentUser._id });
+        const friendPosts = await Promise.all(
+            currentUser.following.map((friendId) => {
+                return Post.find({ userId: friendId });
+            })
+        );
+        res.json(userPosts.concat(...friendPosts));
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
